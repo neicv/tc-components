4150683219cc60ff2684bb6e26a271478114bc39
neicv <neicv@mail.ru>
2021-10-10 03:34:32 +0300

    upd v12

--- src/js/doc/components/IconsInfo.js
+++ src/js/doc/components/IconsInfo.js
@@ -3,11 +3,23 @@ import Component from "../../lib/Component";
 import { ContentCopyIcon } from '../../ui/iconAssets';
 
 const ICON_FILL_COLOR = '#5f6368';
+const DELAY_COPY      = 2000;
+const TEXT_IS_COPED   = 'Copied!';
+const TEXT_ABOUT_COPY = '';
 
 class IconsInfo extends Component {
+    oninit() {
+        this.flagsCopy = {
+            text: false,
+            code: false
+        }
+
+        this.tooltipText = '';
+    }
 
     view() {
         const  { icon } = this.attrs
+
         return (
             <div className={`${this.attrs.className || ''}`}>
                 <figure>
@@ -20,9 +32,24 @@ class IconsInfo extends Component {
                         <pre>
                             <code>{icon.text}</code>
                         </pre>
-                        <span className="tm-infocode-navbtn" onclick={()=> this.copyToClipBoard(icon.text)}>
-                            <span className="tm-icon tm-infocode-icon">
-                            <ContentCopyIcon fill={ICON_FILL_COLOR} />
+                        <span className="tm-infocode-navbtn">
+                            <span 
+                                className="tm-icon tm-infocode-icon"
+                                onclick={()=> this.copyToClipBoard(icon.text, 'text')}
+                            >
+                                <span class="tooltip">
+                                    <span className={`tooltiptext ${this.flagsCopy.text ? 'active' : ''}`}>
+                                        {TEXT_IS_COPED}
+                                    </span>
+                                    <Choose>
+                                        <When condition={this.flagsCopy.text}>
+                                            <i className="font-icon success"></i>
+                                        </When>
+                                        <Otherwise>
+                                            <ContentCopyIcon fill={ICON_FILL_COLOR}/>
+                                        </Otherwise>
+                                    </Choose>
+                                </span>
                             </span>
                         </span>
                     </div>
@@ -37,9 +64,24 @@ class IconsInfo extends Component {
                         <pre>
                             <code>{icon.code}</code>
                         </pre>
-                        <span className="tm-infocode-navbtn" onclick={()=> this.copyToClipBoard(icon.code)}>
-                            <span className="tm-icon tm-infocode-icon">
-                            <ContentCopyIcon fill={ICON_FILL_COLOR} />
+                        <span className="tm-infocode-navbtn">
+                            <span 
+                                className="tm-icon tm-infocode-icon"
+                                onclick={()=> this.copyToClipBoard(icon.code, 'code')}
+                            >
+                                <span class="tooltip">
+                                    <span className={`tooltiptext ${this.flagsCopy.code ? 'active' : ''}`}>
+                                        {TEXT_IS_COPED}
+                                    </span>
+                                    <Choose>
+                                        <When condition={this.flagsCopy.code}>
+                                            <i className="font-icon success"></i>
+                                        </When>
+                                        <Otherwise>
+                                            <ContentCopyIcon fill={ICON_FILL_COLOR}/>
+                                        </Otherwise>
+                                    </Choose>
+                                </span>
                             </span>
                         </span>
                     </div>
@@ -48,8 +90,19 @@ class IconsInfo extends Component {
         );
     }
 
-    copyToClipBoard(value) {
+    copyToClipBoard(value, trigger) {
+        if (this.flagsCopy[trigger]) {
+
+            return;
+        }
+
         navigator.clipboard.writeText(value);
+        this.flagsCopy[trigger] = true;
+
+        setTimeout(() => {
+            this.flagsCopy[trigger] = false;
+            setTimeout(() => m.redraw(), 0);
+        }, DELAY_COPY);
     }
 }
 
